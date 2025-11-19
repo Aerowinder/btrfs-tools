a8627dd7ac664f73e0056c6755a3160a87e0e728
Aerowinder <51070353+Aerowinder@users.noreply.github.com>
2025-11-19 08:54:54 -0500

    v3, combined

---
+++ btrfs-snapshot.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Predefine variables
+TOP_LEVEL=false
+
+usage() {
+    echo
+    echo "USAGE:   $0 -t"
+    echo
+    echo "-t:      Operate in TOP-LEVEL mode. This will store snapshots on the specified subvolume at the DRIVE ROOT."
+    #echo "-s:      Operate in SUB-LEVEL mode. This will store snapshots on the specified subvolume that is nested within the backup target."
+    echo
+    exit 1
+}
+
+# Parse options
+while getopts "t:h" opt; do
+    case $opt in
+        t) TOP_LEVEL=true ;;
+        h) usage ;;
+        *) usage ;;
+    esac
+done
+
+# GLOBAL
+#  NOTE: These variables apply to all running modes.
+BTRFS_SUBVOLUMES=(/ /home) # / = root, /home = home; provide direct path on disk; use a space to separate items
+MAX_SNAPSHOT=8 # Maximum snapshots to retain per snapshotted subvolume
+
+# TOP LEVEL MODE (ROOT-LEVEL STORAGE)
+#  NOTE: In this mode, the snapshot subvolume will NOT be automatically created OR mounted. You MUST do both of these things manually prior to running in this mode.
+TL_MOUNTPOINT="/snapshots"
+
+# NESTED SUBVOL MODE (NESTED STORAGE)
+#  NOTE: In this mode, the snapshot subvolume WILL be automatically created within the target subvolume (ie. /home/.snapshots). No prerequisite setup is required.
+NS_MOUNTPOINT=".snapshots"
+
+
+for subvol in "${BTRFS_SUBVOLUMES[@]}"; do
+    subvol_name="${subvol//\//}" # Strip / out of the path for naming purposes.
+    if [[ -z "$subvol_name" ]]; then # If subvol=/, then set it to "root" instead of empty string.
+        subvol_name="root"
+    fi
+
+    if [[ $TOP_LEVEL == true ]]; then
+        snapshot_root=$TL_MOUNTPOINT
+    else
+        snapshot_root="$subvol/$NS_MOUNTPOINT"
+    fi
+
+    snapshot_name=${subvol_name}-$(date +%Y%m%d-%H%M%S)
+
+    # Check if $subvol exists - checks for misconfiguration of $BTRFS_SUBVOLUMES
+    if [[ ! -d "$subvol" ]]; then
+        echo "[$subvol_name] Error 1: Snapshot source not found."
+        logger -t "btrfs-snapshot" -p user.notice "[$subvol_name] Error 1: Snapshot source not found."
+        continue
+    fi
+
+    # Check if snapshot storage subvolume is accessible to the system.
+    if ! btrfs subvolume show "$snapshot_root" &>/dev/null; then
+        if [[ $TOP_LEVEL == true ]]; then
+            echo "[$subvol_name] Error 2: Snapshot not taken. Either $snapshot_root does not exist or it is not a Btrfs subvolume."
+            logger -t "btrfs-snapshot" -p user.notice "[$subvol_name] Error 2: Snapshot not taken. Either $snapshot_root does not exist or it is not a Btrfs subvolume."
+            continue
+        else
+            if [[ -e "$snapshot_root" ]]; then
+                echo "[$subvol_name] Error 3: Snapshot not taken. $snapshot_root exists but is not a Btrfs subvolume."
+                logger -t "btrfs-snapshot" -p user.notice "Error 3: Snapshot not taken. $snapshot_root exists but is not a Btrfs subvolume."
+                continue
+            fi
+            echo "[$subvol_name] Creating $NS_MOUNTPOINT subvolume on $subvol_name."
+            logger -t "btrfs-snapshot" -p user.notice "[$subvol_name] Creating $NS_MOUNTPOINT subvolume on $subvol_name."
+            btrfs subvolume create "$snapshot_root"
+        fi
+    fi
+
+    # Create read-only snapshot
+    echo "[$subvol_name] Creating snapshot: $snapshot_root/$snapshot_name"
+    logger -t btrfs-snapshot -p user.notice "[$subvol_name] Creating snapshot: $snapshot_root/$snapshot_name"
+    btrfs subvolume snapshot -r "$subvol" "$snapshot_root/$snapshot_name"
+
+    # Cleanup old snapshots
+    # cd "$snapshot_root"
+    # snapshots=($(ls -1d "${subvol_name}-"*/ 2>/dev/null | sort))
+    mapfile -t snapshots < <(find "$snapshot_root" -maxdepth 1 -type d -name "${subvol_name}-*" | sort)
+    count=${#snapshots[@]}
+    if (( count > MAX_SNAPSHOT )); then
+        delete_count=$((count - MAX_SNAPSHOT))
+        for ((i=0; i<delete_count; i++)); do
+            echo "[$subvol_name] Deleting old snapshot: ${snapshots[i]}"
+            logger -t btrfs-snapshot -p user.notice "[$subvol_name] Deleting old snapshot: ${snapshots[i]}"
+            btrfs subvolume delete "$snapshot_root/${snapshots[i]}"
+        done
+    fi
+
+    echo ""
+done
+
+#Changelog
+#2025-11-18 - AS - v1, First release.
+#2025-11-18 - AS - v2, Split into top-level and sub-level versions.
+#2025-11-19 - AS - v3, Combine both top-level and nested-subvol scripts.
